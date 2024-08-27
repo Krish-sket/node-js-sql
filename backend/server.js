18163a4fe7fbb7138f8332b9c9231c715ffd432d
Krish-sket <theworldofgamingksmvd@gmail.com>
2024-08-27 20:47:16 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "remotemysql.com",
+    user: "Rz8hqn1dK4",
+    password: "nd0WKO3xeO",
+    database: "Rz8hqn1dK4"
+});
+connection.connect((err) => {
+    if (err) throw err
+    console.log("connected");
+    var sql = "DROP TABLE Student";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table Dropped DB")
+    });
+});
